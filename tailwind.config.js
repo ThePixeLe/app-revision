11b5adc34d75986cc6967f91fb9b91b772d9cef1
lsoilihi <lsoilihi@arfp.asso.fr>
2025-12-23 21:05:54 +0100

    app revision premier jet

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'algebre': '#3B82F6',
+        'algo': '#8B5CF6',
+        'java': '#10B981',
+        'consolidation': '#F59E0B',
+      }
+    },
+  },
+  plugins: [],
+}
+
